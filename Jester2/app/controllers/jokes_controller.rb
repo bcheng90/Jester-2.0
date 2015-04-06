fd46333ed079b97fb41e238a7fbb64b44f827eaa
Ben Cheng <benjamin.cheng90@gmail.com>
2015-04-06 12:30:53 -0400

    Start new joke page

--- Jester2/app/controllers/jokes_controller.rb
+++ Jester2/app/controllers/jokes_controller.rb
@@ -3,4 +3,17 @@ class JokesController < ApplicationController
   def show
     @joke = Joke.find_by(id: params[:id])
   end
+
+  def new
+  end
+
+  def create
+    new_joke = Joke.new(line1: params[:line1], line2: params[:line2], user_id: current_user.id)
+    if new_joke.save
+      redirect "/jokes/#{new_joke.id}"
+    else
+      [404, "WHY SO SERIOUS?"]
+    end
+  end
+
 end
