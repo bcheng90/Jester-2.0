4d0291c74cdb805245036ee3d56fff80c9d62ae4
Ben Cheng <benjamin.cheng90@gmail.com>
2015-07-06 10:51:48 -0400

    Begin planning on jquery of individual joke page

--- Jester2/app/assets/javascripts/comment.js
+++ Jester2/app/assets/javascripts/comment.js
@@ -9,5 +9,15 @@ $('form#add_comment').on('submit', function(event){
       // I want to go up to comment_list sibling and append the new comment.
       $('#comment_list').append(data)
       $('#add_comment').trigger('reset');
-    });
   });
+});
+// function refreshPartial() {
+//   $.ajax({
+//     url: ''
+//   });
+//   setTimeout(refreshPartial, 5000);
+// }
+
+// $(document).ready(function() {
+//   setTimeout(refreshPartial, 5000);
+// });
